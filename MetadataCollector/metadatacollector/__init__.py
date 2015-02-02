eaa49607d0c236a251defe33d9cdb565891b9ac3
Juan Funez <juan.funez@gmail.com>
2015-02-02 15:31:43 -0200

    primeiras views

--- MetadataCollector/metadatacollector/__init__.py
+++ MetadataCollector/metadatacollector/__init__.py
@@ -6,7 +6,16 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.include('pyramid_debugtoolbar')
+    # home
+    config.add_route('index', '/')
+    # Documentos da Colecao:
+    config.add_route('collection_documents', '/api/v1/collection/{collection_acronym}/document/')
+    # Documentos do Periodico:
+    config.add_route('journal_documents', '/api/v1/journal/{journal_issn}/document/')
+    # Citacoes da Colecao:
+    config.add_route('collection_citations', '/api/v1/collection/{collection_acronym}/citation/')
+    # Citacoes do Periodico:
+    config.add_route('journal_citations', '/api/v1/journal/{journal_issn}/citation/')
     config.scan()
     return config.make_wsgi_app()
--- MetadataCollector/metadatacollector/views.py
+++ MetadataCollector/metadatacollector/views.py
@@ -1,6 +1,45 @@
 from pyramid.view import view_config
+from pyramid.response import Response
+import json
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'MetadataCollector'}
+@view_config(route_name='index', request_method='GET')
+def index(request):
+    return Response('MetadataCollector API')
+
+
+@view_config(route_name='collection_documents', request_method='GET')
+def collection_documents(request):
+    collection_acronym = request.matchdict['collection_acronym']
+    result = {
+        'collection_acronym': collection_acronym,
+    }
+    return Response(json.dumps(result), content_type="application/json")
+
+
+@view_config(route_name='journal_documents', request_method='GET')
+def journal_documents(request):
+    journal_issn = request.matchdict['journal_issn']
+    result = {
+        'journal_issn': journal_issn,
+    }
+    return Response(json.dumps(result), content_type="application/json")
+
+
+@view_config(route_name='collection_citations', request_method='GET')
+def collection_citations(request):
+    collection_acronym = request.matchdict['collection_acronym']
+    result = {
+        'collection_acronym': collection_acronym,
+    }
+    return Response(json.dumps(result), content_type="application/json")
+
+
+@view_config(route_name='journal_citations', request_method='GET')
+def journal_citations(request):
+    journal_issn = request.matchdict['journal_issn']
+    result = {
+        'journal_issn': journal_issn,
+    }
+    return Response(json.dumps(result), content_type="application/json")
+
